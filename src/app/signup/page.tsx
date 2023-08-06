195c8a10e0867a45b3f67ee90af235d6349e26f8
Feecon <mail2feeconbehera@gmail.com>
2023-08-06 19:03:45 +0530

    signedup loggin

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,90 @@
+"use client"
+import Link from 'next/link'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { toNamespacedPath } from 'path';
+import {toast} from'react-hot-toast'
+import axios from 'axios'
+
+export default function SignUpPage() {
+  
+  const router = useRouter();
+
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username:""
+  })
+
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+      setButtonDisabled(false);
+    }
+    else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const [loading, setLoading] = React.useState(false);
+  
+  const onSignUp = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/users/signup', user);
+      console.log("SignUp success", response.data);
+      router.push("/login")
+    } catch (error:any) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <div className = "flex flex-col items-center justify-center min-h-screen py-2">
+        <h1>{ loading?"Processing": "SignUp"}</h1>
+        <hr />
+        <label htmlFor = "username"> username </label>
+        <input 
+          className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+            id="username"
+            type="text"
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+          placeholder='username'
+        />
+        
+        <label htmlFor = "email"> email </label>
+        <input 
+          className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+            id="email"
+            type="text"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder='email'
+        />
+        
+        <label htmlFor = "password"> password </label>
+        <input 
+          className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black'
+            id="password"
+            type="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder='password'
+        />
+
+        <button
+          onClick={onSignUp}
+          className='p-2 border mb-4 rounded-lg focus:outline-none focus:outline-gray-600'
+        >{buttonDisabled?"No Signup":"Signup" }</button>
+        <Link href={'/login'}>Visit login here</Link>
+          
+      </div>
+    
+    </> 
+  )
+}
---
+++ src/models/userModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+import React from 'react'
+
+const userSchema = new mongoose.Schema({
+    username:   {
+        type: String,
+        required: [true, 'pleaase provide a username'],
+        unique:true,
+    },
+    email: {
+        type: String,
+        required: [true, 'please provide a email'],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'please provide a email'],
+        
+    },
+    isVerified: {
+        type: Boolean,
+        default:false,
+    },
+    isAdmin: {
+        type: Boolean,
+        defailt:false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry:Date,
+    
+})
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
